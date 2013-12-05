82af60b8a6529c91240aee5a7241547cae117b08
shridhar-v <shridharv@fidelis-inc.com>
2013-12-05 14:54:12 +0530

    First commit
    tst

---
+++ src/test/java/SeleniumClientFactoryOnDemandTest.java
@@ -0,0 +1,41 @@
+import com.saucelabs.selenium.client.factory.SeleniumFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Ross Rowe
+ */
+public class SeleniumClientFactoryOnDemandTest {
+
+    private WebDriver webDriver;
+
+    /**
+     * Create a WebDriver instance using Selenium Client Factory.  We don't have to refer to the environment
+     * variables set by the CI plugin, as that's handled by the Selenium Client Factory logic.  We also don't have
+     * to output the Sauce OnDemand Session id.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        webDriver = SeleniumFactory.createWebDriver();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        webDriver.quit();
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void fullRun() throws Exception {
+        webDriver.get("http://www.amazon.com/");
+        assertEquals("Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more", webDriver.getTitle());
+    }
+}
